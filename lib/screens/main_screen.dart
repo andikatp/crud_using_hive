f9b3bc4f3a8bf3737e29a866711f7a580c7a4762
AndikaTP <andikamufc@gmail.com>
2022-12-01 12:41:06 +0700

    fix keyboard covering the text box when editing

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
+import 'package:hive_flutter/adapters.dart';
 
 class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
@@ -11,156 +12,150 @@ class MainScreen extends StatefulWidget {
 class _MainScreenState extends State<MainScreen> {
   final TextEditingController _nameController = TextEditingController();
   final TextEditingController _quantityController = TextEditingController();
-  final _hiveLearn = Hive.box('hiveLearn');
+  final _itemBox = Hive.box('hiveLearn');
   List<Map<String, dynamic>> _items = [];
 
   Future<void> _refreshItem() async {
-    final data = _hiveLearn.keys.map((key) {
-      final item = _hiveLearn.get(key);
-      return {"key": key, "name": item["name"], "quantity": item["quantity"]};
+    final data = _itemBox.keys.map((key) {
+      final item = _itemBox.get(key);
+      return {'key': key, 'name': item['name'], 'quantity': item['quantity']};
     }).toList();
-
     setState(() {
       _items = data.reversed.toList();
     });
   }
 
-  Future<void> _createItem(Map<String, dynamic> newItem) async {
-    await _hiveLearn.add(newItem);
+  void _updateItem(int key, Map<String, dynamic> item) {
+    _itemBox.put(key, item);
     _refreshItem();
+    Navigator.of(context).pop();
   }
 
-  Future<void> _updateItem(int itemKey, Map<String, dynamic> item) async {
-    await _hiveLearn.put(itemKey, item);
+  void _createItem(Map<String, dynamic> item) {
+    _itemBox.add(item);
     _refreshItem();
+    Navigator.of(context).pop();
   }
 
-  Future<void> _deleteItem(int itemKey) async {
-    await _hiveLearn.delete(itemKey);
+  void _deleteItem(int key) {
+    _itemBox.delete(key);
     _refreshItem();
-    if (!context.mounted) return;
-    ScaffoldMessenger.of(context)
-        .showSnackBar(const SnackBar(content: Text('Item has been deleted')));
+    if (!mounted) return;
+    ScaffoldMessenger.of(context).showSnackBar(
+      const SnackBar(
+        content: Text('Deleting Product'),
+      ),
+    );
   }
 
-  void _showForm(BuildContext ctx, int? itemKey) async {
-    if (itemKey != null) {
-      final existingItem =
-          _items.firstWhere((element) => element['key'] == itemKey);
+  void _showForm(BuildContext ctx, int? key) async {
+    if (key != null) {
+      final existingItem = _items.firstWhere((item) => item['key'] == key);
       _nameController.text = existingItem['name'];
       _quantityController.text = existingItem['quantity'];
     }
 
     showModalBottomSheet(
       context: ctx,
-      elevation: 5,
       isScrollControlled: true,
+      elevation: 5,
       builder: (context) {
         return Container(
           padding: EdgeInsets.only(
-              bottom: MediaQuery.of(ctx).viewInsets.bottom,
-              top: 15,
-              left: 15,
-              right: 15),
+            bottom: MediaQuery.of(context).viewInsets.bottom,
+            right: 15,
+            top: 15,
+            left: 15,
+          ),
           child: Column(
-              mainAxisSize: MainAxisSize.min,
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: [
-                TextField(
-                  autofocus: true,
-                  controller: _nameController,
-                  decoration: const InputDecoration(hintText: 'Name'),
-                ),
-                const SizedBox(height: 10),
-                TextField(
-                  controller: _quantityController,
-                  decoration: const InputDecoration(hintText: 'Quantity'),
-                  keyboardType: TextInputType.number,
-                ),
-                const SizedBox(height: 10),
-                ElevatedButton(
-                    onPressed: () {
-                      if (itemKey != null) {
-                        _updateItem(itemKey, {
-                          'name': _nameController.text.trim(),
-                          'quantity': _quantityController.text.trim()
-                        });
-                      }
-                      if (itemKey == null) {
-                        _createItem({
-                          'name': _nameController.text,
-                          'quantity': _quantityController.text
-                        });
-                      }
-                      //clear text field
-                      _nameController.text = '';
-                      _quantityController.text = '';
-                      Navigator.pop(ctx);
-                    },
-                    child: itemKey == null
-                        ? const Text('Create New')
-                        : const Text('Update')),
-                const SizedBox(height: 10),
-              ]),
+            crossAxisAlignment: CrossAxisAlignment.end,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              TextField(
+                controller: _nameController,
+                autofocus: true,
+                decoration: const InputDecoration(
+                    border: OutlineInputBorder(), hintText: 'Name'),
+              ),
+              const SizedBox(height: 5),
+              TextField(
+                controller: _quantityController,
+                autofocus: true,
+                decoration: const InputDecoration(
+                    border: OutlineInputBorder(), hintText: 'Quantity'),
+                keyboardType: TextInputType.number,
+              ),
+              const SizedBox(height: 5),
+              ElevatedButton(
+                onPressed: () {
+                  if (key != null) {
+                    _updateItem(key, {
+                      'name': _nameController.text.trim(),
+                      'quantity': _quantityController.text.trim()
+                    });
+                  }
+                  if (key == null) {
+                    _createItem({
+                      'name': _nameController.text.trim(),
+                      'quantity': _quantityController.text.trim(),
+                    });
+                  }
+                  _nameController.text = '';
+                  _quantityController.text = '';
+                },
+                child:
+                    key == null ? const Text('Create') : const Text('Update'),
+              ),
+            ],
+          ),
         );
       },
     );
   }
 
   @override
-  void dispose() {
-    _nameController.dispose();
-    _quantityController.dispose();
-    super.dispose();
-  }
-
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Hive'),
+        title: const Text('Learning Hive'),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () => _showForm(context, null),
         child: const Text('+'),
       ),
       body: FutureBuilder(
-        future: _refreshItem(),
-        builder: (context, snapshot) => snapshot.connectionState ==
-                ConnectionState.waiting
-            ? const Center(
-                child: CircularProgressIndicator(),
-              )
-            : ListView.builder(
-                itemCount: _items.length,
-                itemBuilder: (_, index) {
-                  final currentItem = _items[index];
-                  return Card(
-                    color: Colors.orange.shade100,
-                    margin: const EdgeInsets.all(10),
-                    elevation: 3,
-                    child: ListTile(
-                      title: Text(
-                        currentItem['name'],
-                      ),
-                      subtitle: Text(currentItem['quantity'].toString()),
-                      trailing: Row(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          IconButton(
-                              onPressed: () =>
-                                  _showForm(context, currentItem['key']),
-                              icon: const Icon(Icons.edit)),
-                          IconButton(
-                              onPressed: () => _deleteItem(currentItem['key']),
-                              icon: const Icon(Icons.delete))
-                        ],
-                      ),
-                    ),
-                  );
-                },
-              ),
-      ),
+          future: _refreshItem(),
+          builder: (context, snapshot) =>
+              snapshot.connectionState == ConnectionState.waiting
+                  ? const Center(child: CircularProgressIndicator())
+                  : ListView.builder(
+                      itemCount: _items.length,
+                      itemBuilder: (context, index) {
+                        final currentItem = _items[index];
+                        return Card(
+                          margin: const EdgeInsets.all(20),
+                          elevation: 3,
+                          color: Colors.orange.shade100,
+                          child: ListTile(
+                            title: Text(currentItem['name']),
+                            subtitle: Text(currentItem['quantity']),
+                            trailing: Row(
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                IconButton(
+                                    onPressed: () =>
+                                        _showForm(context, currentItem['key']),
+                                    icon: const Icon(Icons.edit)),
+                                IconButton(
+                                    onPressed: () =>
+                                        _deleteItem(currentItem['key']),
+                                    icon: const Icon(Icons.delete)),
+                              ],
+                            ),
+                          ),
+                        );
+                      },
+                    )),
     );
   }
 }
